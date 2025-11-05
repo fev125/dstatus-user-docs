f849e46e234642c85d8720cf6d477f54d57c7227
fev125 <731312580@qq.com>
2025-11-06 01:31:40 +0800

    菜单更新

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -36,6 +36,7 @@ export default defineConfig({
       { text: '首页', link: '/' },
       { text: '快速开始', link: '/quick-start' },
       { text: '使用指南', link: '/usage' },
+      { text: '官网', link: 'https://client.vps.mom/', target: '_blank', rel: 'noopener noreferrer' },
     ],
     
     // 侧边栏配置
